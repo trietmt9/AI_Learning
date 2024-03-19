f365c634cb36d4187e5302ae684040ca60b23175
Triet <trietmt9@gmail.com>
2024-03-19 11:55:04 +0800

    graph

--- Support_Vector_Machine/main.py
+++ Support_Vector_Machine/main.py
@@ -49,6 +49,22 @@ svm_clf.fit(X_train,y_train)
 #--- Test the SVM model ---
 y_hat=svm_clf.predict(X_test)
 
+
+#--- Plot the decision boundary ---
+
+import matplotlib.pyplot as plt
+from PlotClassification import plot_decision_regions
+
+import numpy as np
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=svm_clf, test_idx=range(115, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+print(work_path)
 print("This is train X value: ")
 print(np.array(X_train))
 print("This is y hat value")
